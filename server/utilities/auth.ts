e977e341e2d3897176c7bec853a0ae8c4a0309ee
man <>
2025-04-29 17:31:17 +0300

    addi mac support -1

--- server/utilities/auth.ts
+++ server/utilities/auth.ts
@@ -1,11 +1,9 @@
 import { Globals } from '../globals';
+import { isWindows, isMac, isLinux } from './host';
+import { exec } from 'child_process';
+import { promisify } from 'util';
 
-import {
-isWindows,
-isMac,
-isLinux,
-} from './host';
-
+const execPromise = promisify(exec);
 
 export async function isAdmin(): Promise<boolean> {
   if (isWindows()) {
@@ -14,18 +12,24 @@ export async function isAdmin(): Promise<boolean> {
   } else if (isLinux()) {
     return process.getuid !== undefined && process.getuid() === 0;
   } else if (isMac()) {
-    console.log('Running on Mac');
+    try {
+      const { stdout } = await execPromise('id -u');
+      return parseInt(stdout.trim(), 10) === 0; // 0 indicates root user
+    } catch (error) {
+      console.error('Error checking admin status on Mac:', error);
+      return false;
+    }
   }
   
   return false;
 }
 
-
 export async function isFromManager(origin: string): Promise<boolean> {
   if (
     origin ||
     origin.startsWith(process.env.SERVER_IP + ':' + process.env.SERVER_PORT)
-  )
+  ) {
     return true;
+  }
   return false;
 }
