05fa8fb2fc997b4491c39902b04dae5cc57eda5a
Master Chief <46121368+MasterChiefProject@users.noreply.github.com>
2025-05-11 12:14:21 +0300

    init honeytoken

--- routes/general.ts
+++ routes/general.ts
@@ -1,8 +1,20 @@
 import { Router } from 'express';
 import { Globals } from '../globals.ts';
+import { initHoneytokens } from '../utilities/init.ts';
 
 export function serveGeneral() {
   const router = Router();
+
+  router.get('/general/init', async (req, res) => {
+    try {
+      await initHoneytokens();
+      res.json({ success: true });
+      return;
+    } catch {
+      res.json({ success: false });
+      return;
+    }
+  });
 
   Globals.app.use('/', router);
 }
