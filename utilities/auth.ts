33b22c74f102fb76fdc6b1b92243f004f04215a7
Master Chief <46121368+MasterChiefProject@users.noreply.github.com>
2025-05-17 01:54:47 +0300

    Update auth.ts

--- utilities/auth.ts
+++ utilities/auth.ts
@@ -4,13 +4,13 @@ export async function isAdmin(): Promise<boolean> {
   if (isWindows()) {
     const isElevated = await import('is-elevated');
     return await isElevated.default();
-  } else if (isLinux()) {
-    return process.getuid !== undefined && process.getuid() === 0;
-  } else if (isMac()) {
-    console.log('Running on Mac');
+  } else if (isLinux() || isMac()) {
+    // On POSIX (Linux & macOS), root has UID 0
+    return typeof process.getuid === 'function' && process.getuid() === 0;
   }
   return false;
 }
+
 
 export async function isFromManager(origin: string): Promise<boolean> {
   if (origin || origin.startsWith(process.env.MANAGER_IP + ':' + process.env.MANAGER_PORT)) return true;
