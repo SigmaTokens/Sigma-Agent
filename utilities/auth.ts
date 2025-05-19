2a98dd69e6b94ca38ee54644611139ba1ff5d524
Master Chief <46121368+MasterChiefProject@users.noreply.github.com>
2025-05-19 15:49:28 +0300

    macos = mac

--- utilities/auth.ts
+++ utilities/auth.ts
@@ -5,12 +5,11 @@ export async function isAdmin(): Promise<boolean> {
     const isElevated = await import('is-elevated');
     return await isElevated.default();
   } else if (isLinux() || isMac()) {
-    // On POSIX (Linux & macOS), root has UID 0
+    // On POSIX (Linux & mac), root has UID 0
     return typeof process.getuid === 'function' && process.getuid() === 0;
   }
   return false;
 }
-
 
 export async function isFromManager(origin: string): Promise<boolean> {
   if (origin || origin.startsWith(process.env.MANAGER_IP + ':' + process.env.MANAGER_PORT)) return true;
