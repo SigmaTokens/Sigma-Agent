71fce8fba9c98ea2cd127e7a82f35caadb48d773
Daniel Nagornov <Danieln056@gmail.com>
2025-06-06 00:59:54 +0300

    .

--- classes/api/honeytoken_api.ts
+++ classes/api/honeytoken_api.ts
@@ -1,16 +1,38 @@
 import { HoneytokenType } from '../../utilities/typing.ts';
 import { Honeytoken } from '../abstract/Honeytoken.ts';
 import { API_route } from '../../utilities/typing.ts';
+import express from 'express';
 
 export class Honeytoken_API extends Honeytoken {
   api_port: number;
   apis: API_route[];
   is_monitoring: boolean = false;
+  sub_application: any;
 
   private constructor(group_id: string, expirationDate: Date, grade: number, api_port: number, apis: API_route[]) {
     super('', group_id, HoneytokenType.API, expirationDate, grade, '');
     this.api_port = api_port;
     this.apis = apis;
+
+    this.sub_application = express();
+    this.sub_application.use(express.json());
+
+    apis.forEach(({ method, route, response }: API_route) => {
+      this.sub_application[method.toLowerCase()](route, (req: any, res: any) => {
+        if (this.is_monitoring) {
+          // TODO: do some monitoring
+
+          // send alert back to the manager
+          res.status(200).json(response);
+        } else {
+          res.status(500);
+        }
+      });
+    });
+
+    this.sub_application.listen(api_port, () => {
+      console.log(`API sub application listening on port ${api_port}`);
+    });
   }
 
   public static async create(
@@ -25,10 +47,10 @@ export class Honeytoken_API extends Honeytoken {
   }
 
   startMonitor() {
-    //
+    this.is_monitoring = false;
   }
 
   stopMonitor() {
-    //
+    this.is_monitoring = true;
   }
 }
